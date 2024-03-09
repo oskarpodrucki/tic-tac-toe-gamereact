6e8d5c877d591e7ea4ec2cdafbf644be1232f5c7
OskarPodrucki <oskar.podrucki1@gmail.com>
2024-03-09 12:53:44 +0100

    ...

--- src/Board.js
+++ src/Board.js
@@ -1,12 +1,9 @@
 import { useState } from "react";
 import Square from "./Square";
 
-export default function Board() {
-	const [xIsNext, setXIsNext] = useState(true);
-	const [squares, setSquares] = useState(Array(9).fill(null));
-
+export default function Board({ xIsNext, squares, onPlay }) {
 	function handleClick(i) {
-		if (squares[i] || calculateWinner(squares)) {
+		if (calculateWinner(squares) || squares[i]) {
 			return;
 		}
 		const nextSquares = squares.slice();
@@ -15,8 +12,7 @@ export default function Board() {
 		} else {
 			nextSquares[i] = "O";
 		}
-		setSquares(nextSquares);
-		setXIsNext(!xIsNext);
+		onPlay(nextSquares);
 	}
 
 	const winner = calculateWinner(squares);
